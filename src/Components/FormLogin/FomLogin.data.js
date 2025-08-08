7fdde8d38f9cd3e2bc0bb7510e2238bb4899bb2e
Micael1889 <Ramellomicael2000@gmail.com>
2025-08-07 21:56:53 -0300

    Empezamos

---
+++ src/Components/FormLogin/FomLogin.data.js
@@ -0,0 +1,14 @@
+export const initFormData = {
+  usuario: "",
+  contrasena: "",
+  correo: " ",
+  nombre: "",
+  apellido: "",
+  repetirContrasena: "",
+  telefono: "",
+};
+
+export const initErrores = {
+  usuario: false,
+  contrasena: false,
+};
